cf390a5f3c4ec5ac9b3e49a6a6f530de2dca3250
kwalden2018 <waldenka@grinnell.edu>
2018-10-04 13:30:39 -0500

    Burrito is spicy.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
-let arrayOfStrings, arrayOfNumbers, arrayMixed;
-arrayOfStrings = ["a", "b", "c"];
-arrayOfNumbers = [1, 2, 3];
-arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
-$("#response").html(arrayOfStrings[2]);
+let myHabaneroSauceSquirts, myBurritoObject;
+// First, define and assign a variable for how
+// spicy the burrito is.
+myHabaneroSauceSquirts = 3;
+// Now assign the burrito object.
+myBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  // Make use of the variable above.
+  habaneroSauceSquirts: myHabaneroSauceSquirts,
+  // Use the variable again in a function.
+  spiciness: function(){
+    if (myHabaneroSauceSquirts > 0 ){
+      alert("This is a spicy burrito!");
+    } else {
+      alert("This is a mild burrito.");
+    }
+  }
+};
+$("#response").html("Your burrito has " +
+  myBurritoObject.habaneroSauceSquirts +
+  " squirts of habanero.");
+myBurritoObject.spiciness();
