99bffd71ac7fab64e1f3ac0d514ebf638fed681c
kwalden2018 <waldenka@grinnell.edu>
2018-10-04 13:18:05 -0500

    First array test

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
-let tipCalculator;
-tipCalculator = function(total, tipRate){
-  let tipAmount;
-  tipAmount = tipRate * total;
-  $("#response").html("Your tip is $" + tipAmount);
-};
-tipCalculator(50.00,0.2);
+let arrayOfStrings, arrayOfNumbers, arrayMixed;
+arrayOfStrings = ["a", "b", "c"];
+arrayOfNumbers = [1, 2, 3];
+arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
+$("#response").html(arrayOfStrings[2]);
